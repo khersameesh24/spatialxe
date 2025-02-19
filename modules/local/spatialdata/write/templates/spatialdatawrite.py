d8c42f21e95105ec13191b8b974c58f092f6ba5b
f641l <florian.heyl@dkfz-heidelberg.de>
2025-02-19 13:32:49 +0100

    adding spatialdatawrite.py scripts

---
+++ modules/local/spatialdata/write/templates/spatialdatawrite.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""Provide functions to write spatialdata object from segmentation format."""
+
+import sys
+import spatialdata
+from spatialdata_io import xenium
+
+def main():
+    """Write spatialdata object folder."""
+
+    print("[START]")
+
+    input_path = "${bundle}"
+    output_path = "${meta.id}"
+
+    format = "${params.format}"
+
+    if ( format == "xenium" ):
+        sd_xenium_obj = xenium(
+            input_path,
+            cells_as_circles=True,
+            nucleus_boundaries=True,
+            transcripts=True,
+            morphology_mip=True,
+            morphology_focus=True,
+            n_jobs="${task.cpus}",
+        )
+        print(sd_xenium_obj)
+        sd_xenium_obj.write(f"{output_path}/spatialdata")
+    else:
+        sys.exit("[ERROR] Format not found")
+
+    #Output version information
+    with open("versions.yml", "w") as f:
+        f.write('"${task.process}":\\n')
+        f.write(f'spatialdata: "{spatialdata.__version__}"\\n')
+
+    print("[FINISH]")
+
+if __name__ == "__main__":
+    main()
